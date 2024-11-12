3bec720cce8d64d9adf1bd84b98836f6f8c205c9
sourav jaiswal <souravjaiswal2000@gmail.com>
2024-11-12 15:15:50 +0530

    Refactor

---
+++ DSA/LeetcodeDaily/2070. Most Beautiful Item for Each Query.java
@@ -0,0 +1,41 @@
+package LeetcodeDaily;
+
+import java.util.Arrays;
+
+/* First solution
+* TC: NLOGN + KLOGK + K + K  = O( NLogN + KLogK )
+* SC: K + K = O(K) + some extra sorting space 
+*/
+
+class Solution {
+    public int[] maximumBeauty(int[][] items, int[] queries) {
+
+        int n = items.length, k = queries.length;
+
+        int ans[] = new int[k];
+        int qWithIndex[][] = new int[k][2];
+
+        for (int i = 0; i < k; i++) {
+            qWithIndex[i][0] = queries[i];
+            qWithIndex[i][1] = i;
+        }
+
+        Arrays.sort(items, (a, b) -> a[0] - b[0]);
+        Arrays.sort(qWithIndex, (a, b) -> a[0] - b[0]);
+
+        int idx = 0;
+        int maxBeauty = 0;
+
+        for (int i = 0; i < k; i++) {
+            int price = qWithIndex[i][0];
+
+            while (idx < n && items[idx][0] <= price) {
+                maxBeauty = Math.max(maxBeauty, items[idx][1]);
+                idx++;
+            }
+
+            ans[qWithIndex[i][1]] = maxBeauty;
+        }
+        return ans;
+    }
+}

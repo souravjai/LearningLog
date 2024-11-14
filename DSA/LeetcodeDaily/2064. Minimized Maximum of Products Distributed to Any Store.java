6fec8aa757c77f8520a88321f0df72fddccecb28
sourav jaiswal <souravjaiswal2000@gmail.com>
2024-11-14 14:45:37 +0530

    LeetcodeDaily

---
+++ DSA/LeetcodeDaily/2064. Minimized Maximum of Products Distributed to Any Store.java
@@ -0,0 +1,23 @@
+package DSA.LeetcodeDaily;
+
+import java.util.PriorityQueue;
+
+class Solution {
+    public int minimizedMaximum(int n, int[] quantities) {
+        PriorityQueue<long[]> pq = new PriorityQueue<>((a, b) -> Long.compare(b[0] * a[1], b[1] * a[0]));
+        long[] cur;
+
+        int m = quantities.length;
+        for (int i = 0; i < m; i++) {
+            pq.offer(new long[] { quantities[i], 1 });
+        }
+
+        for (int i = 0; i < n - m; i++) {
+            cur = pq.poll();
+            pq.offer(new long[] { cur[0], cur[1] + 1 });
+        }
+
+        cur = pq.poll();
+        return (int) Math.ceil((cur[0] * 1.0) / cur[1]);
+    }
+}
---
+++ DSA/LeetcodeDaily/FindDuplicateCharacterInAString.java
@@ -0,0 +1,24 @@
+package DSA.LeetcodeDaily;
+
+
+class Test {
+    public static void main(String[] args) {
+        String s = "ABBCBC";
+
+        int freq[] = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = Character.toLowerCase(s.charAt(i));
+            int alphabetPos = ch - 'a';
+
+            freq[alphabetPos]++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 1) {
+                System.out.println((char) ('a' + i));
+            }
+        }
+
+    }
+}

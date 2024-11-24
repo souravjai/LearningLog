f8cb5babf82f0120066de667a54d3c605c3fb1ff
sourav jaiswal <souravjaiswal2000@gmail.com>
2024-11-24 14:23:29 +0530

    l75 24/11

--- DSA/Leetcode75/735. Asteroid Collision.cpp
+++ DSA/Leetcode75/735. Asteroid Collision.cpp
@@ -1,24 +1,36 @@
-class Solution {
-    public:
-        vector<int> asteroidCollision(vector<int>& asteroids) {
-            int n = asteroids.size();
-    
-            vector<int> ans;
-    
-            for(int i =0;i<n;i++){
-                int cur = asteroids[i];
-                while(ans.size()!=0 && ans.back()>0 && cur<0){
-                    if(ans.back()<abs(cur)){
-                        ans.pop_back();
-                    }else if(ans.back()==abs(cur)){
-                        ans.pop_back();
-                        cur=0;
-                    }else{
-                        cur=0;
-                    }
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> asteroidCollision(vector<int> &asteroids)
+    {
+        int n = asteroids.size();
+
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++)
+        {
+            int cur = asteroids[i];
+            while (ans.size() != 0 && ans.back() > 0 && cur < 0)
+            {
+                if (ans.back() < abs(cur))
+                {
+                    ans.pop_back();
                 }
-                if(cur!=0) ans.push_back(cur);
+                else if (ans.back() == abs(cur))
+                {
+                    ans.pop_back();
+                    cur = 0;
+                }
+                else
+                {
+                    cur = 0;
+                }
             }
-            return ans;
+            if (cur != 0)
+                ans.push_back(cur);
         }
-    };
+        return ans;
+    }
+};

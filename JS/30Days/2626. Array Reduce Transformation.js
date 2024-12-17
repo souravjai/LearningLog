050e42038dd40317f140aec405cb80cf7e5cf1b1
sourav jaiswal <souravjaiswal2000@gmail.com>
2024-12-18 00:12:10 +0530

    js

--- JS/30Days/2626. Array Reduce Transformation.js
+++ JS/30Days/2626. Array Reduce Transformation.js
@@ -1,3 +1,32 @@
+/** 
+I came across a article that explained reduce more throughly
+ */
+/**
+ * @template T, U
+ * @param {(previousValue: U, currentValue: T, currentIndex: number, array: T[]) => U} callbackFn
+ * @param {U} [initialValue]
+ * @return {Array<U>}
+ */
+ Array.prototype.myReduce = function (callbackFn, initialValue) {
+  
+    if(initialValue === undefined && this.length === 0){
+      throw TypeError("Reduce of empty array with no initial value") 
+    }
+  
+    let retVal = initialValue !== undefined ? initialValue : this[0];
+    let index = initialValue !== undefined ? 0 : 1;
+    
+    for(;index<this.length;index++){
+      if(this.hasOwnProperty(index))
+        retVal = callbackFn(retVal,this[index],index,this);
+    }
+  
+    return retVal;
+  };
+
+
+
+
 /**
  * @param {number[]} nums
  * @param {Function} fn

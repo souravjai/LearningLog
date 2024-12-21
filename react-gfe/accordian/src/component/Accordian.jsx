6469e881ce87b98885d3b1cdfbcbb23cdd1554c4
sourav jaiswal <souravjaiswal2000@gmail.com>
2024-12-21 14:40:29 +0530

    Added Accessbility

--- react-gfe/accordian/src/component/Accordian.jsx
+++ react-gfe/accordian/src/component/Accordian.jsx
@@ -1,11 +1,17 @@
 import react, { useState } from "react";
 
+const allowedKeys = ["Enter", " "];
+
 function Accordian({ label, content }) {
 	const [isCollapsed, setIsCollapsed] = useState(true);
 	return (
 		<div className='accordian' aria-expanded={!isCollapsed}>
 			<div
 				onClick={() => setIsCollapsed((prev) => !prev)}
+				onKeyDown={(e) =>
+					allowedKeys.includes(e.key) && setIsCollapsed((prev) => !prev)
+				}
+				tabIndex={0}
 				className='accordian__ele'>
 				<span className='accordian__label'>{label}</span>
 				<span className='arrow'>➡️</span>
